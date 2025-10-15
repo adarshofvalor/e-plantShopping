aaa3e0243c0455cbbd40de527b36843705540e92
adarshofvalor <adarshdevol@xharvoc.com>
2025-10-15 02:28:24 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping/", // <-- match your repo name exactly
+  base: "/shoppingreact",
   plugins: [react()],
+  build: { outDir: 'dist', emptyOutDir: true, }
 })
